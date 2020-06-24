960761b2909a073c56ab0e3d7945f9686531b37a
yaofang <yaofang@192.168.140.1>
2020-06-24 23:18:16 +0800

    修改分支

--- Git_Demo/src/main/java/com/yaof/GitDemoApplication.java
+++ Git_Demo/src/main/java/com/yaof/GitDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Hello world!
+ * springboot 启动文件
  *
  */
 @SpringBootApplication
