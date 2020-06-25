e91feec9f1da5aa617c1e0a68a3c08ef68326656
yaofang <yaofang@192.168.140.1>
2020-06-25 23:47:55 +0800

    添加数据源

---
+++ Git_Demo/src/main/java/com/yaof/mapper/TestMapping.java
@@ -0,0 +1,11 @@
+package com.yaof.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface TestMapping {
+
+	public int getCount();
+}
--- Git_Demo/src/main/java/com/yaof/service/impl/TestServiceImpl.java
+++ Git_Demo/src/main/java/com/yaof/service/impl/TestServiceImpl.java
@@ -1,14 +1,20 @@
 package com.yaof.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.yaof.mapper.TestMapping;
 import com.yaof.service.TestService;
 
 @Service
 public class TestServiceImpl implements TestService {
 
+	@Autowired
+	private TestMapping testMapping;
+	
 	@Override
 	public String getTestString() {
+		System.out.println(testMapping.getCount());
 		return "test service result";
 	}
 
